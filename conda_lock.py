15ea703f4f2f37015e4d5103f3ea23974ccd80b9
Marius van Niekerk <marius.v.niekerk@gmail.com>
2019-09-11 18:06:27 -0500

    Abuse environment variables instead of vendoring hunks of conda install

--- conda_lock.py
+++ conda_lock.py
@@ -1,97 +1,40 @@
 """
 Somewhat hacky solution to create conda lock files.
-
-This HAS to be executed from a python that has conda installed into its environment
-    
-TODO: This might be doable using to perform the inial solve withing resorting to copying over
-    the solver from conda, but presently there is no way to have conda create use the solve 
-    of another platform
-
-    `conda create -n foo --json --dry-run specs
-
 """
 from __future__ import absolute_import, print_function
 
+import json
 import pathlib
+import subprocess
 import sys
 import yaml
-
-from conda.base.constants import UpdateModifier
-from conda.base.context import context, reset_context
-from conda.core.solve import DepsModifier, Solver
-from conda.exceptions import (
-    PackagesNotFoundError,
-    ResolvePackageNotFound,
-    SpecsConfigurationConflictError,
-    UnsatisfiableError,
-)
+import os
 
 DEFAULT_PLATFORMS = ["osx-64", "linux-64", "win-64"]
-PREFIX_NAME = "__magicmarker"
+FAKE_PREFIX_NAME = "__magicmarker"
+FAKE_PKGS_ROOT = "/something/that/does/not/exist"
 
 
 def solve_specs_for_arch(channels, specs, platform):
-    # Harvested liberally from conda install 
-    context.validate_configuration()
+    # type: (typing.List[str], typing.List[str], str) -> dict
+    args = [
+        "conda",
+        "create",
+        "-p",
+        FAKE_PREFIX_NAME,
+        "--override-channels",
+        "--dry-run",
+        "--json",
+    ]
+    for channel in channels:
+        args.extend(["-c", channel])
+    args.extend(specs)
 
-    repodata_fns = context.repodata_fns
-    _should_retry_unfrozen = True
-
-    for repodata_fn in repodata_fns:
-        try:
-            solver = Solver(
-                prefix=PREFIX_NAME,
-                channels=channels,
-                subdirs=(platform, "noarch"),
-                specs_to_add=specs,
-                repodata_fn=repodata_fn,
-            )
-            deps_modifier = context.deps_modifier
-            update_modifier = UpdateModifier.FREEZE_INSTALLED
-
-            unlink_link_transaction = solver.solve_for_transaction(
-                deps_modifier=deps_modifier,
-                update_modifier=update_modifier,
-                force_reinstall=False,
-                should_retry_solve=(
-                    _should_retry_unfrozen or repodata_fn != repodata_fns[-1]
-                ),
-            )
-
-            break
-        except (ResolvePackageNotFound, PackagesNotFoundError) as e:
-            # end of the line.  Raise the exception
-            if repodata_fn == repodata_fns[-1]:
-                # PackagesNotFoundError is the only exception type we want to raise.
-                #    Over time, we should try to get rid of ResolvePackageNotFound
-                if isinstance(e, PackagesNotFoundError):
-                    raise e
-
-        except (UnsatisfiableError, SystemExit, SpecsConfigurationConflictError):
-            if _should_retry_unfrozen:
-                try:
-                    unlink_link_transaction = solver.solve_for_transaction(
-                        deps_modifier=deps_modifier,
-                        update_modifier=UpdateModifier.UPDATE_SPECS,
-                        force_reinstall=context.force_reinstall or context.force,
-                        should_retry_solve=(repodata_fn != repodata_fns[-1]),
-                    )
-                except (
-                    UnsatisfiableError,
-                    SystemExit,
-                    SpecsConfigurationConflictError,
-                ) as e:
-                    if repodata_fn != repodata_fns[-1]:
-                        continue
-                    else:
-                        raise
-
-            elif repodata_fn != repodata_fns[-1]:
-                continue
-            else:
-                raise
-
-    return unlink_link_transaction
+    env = dict(os.environ)
+    env.update({"CONDA_SUBDIR": platform, "CONDA_PKGS_DIRS": FAKE_PKGS_ROOT})
+    json_output = subprocess.check_output(args, encoding="utf-8", env=env)
+    # print(json_output)
+    return json.loads(json_output)
 
 
 def parse_environment_file(environment_file):
@@ -100,23 +43,32 @@ def parse_environment_file(environment_file):
         raise FileNotFoundError("environment.yml not found")
     with environment_file.open("r") as fo:
         env_yaml_data = yaml.safe_load(fo)
-    # TODO: we basically ignore most of the field for now.
+    # TODO: we basically ignore most of the fields for now.
     #       notable pip deps are not supported
     specs = env_yaml_data["dependencies"]
     channels = env_yaml_data.get("channels", [])
     return {"specs": specs, "channels": channels}
 
 
+def fn_to_dist_name(fn):
+    fn, _, _ = fn.partition('.conda')
+    fn, _, _ = fn.partition('.tar.bz2')
+    return fn
+
 def make_lock_files(platforms, channels, specs):
     for platform in platforms:
         print("generating lockfile for {}".format(platform), file=sys.stderr)
-        state = solve_specs_for_arch(platform=platform, channels=channels, specs=specs)
+        dry_run_install = solve_specs_for_arch(
+            platform=platform, channels=channels, specs=specs
+        )
         with open("conda-{}.lock".format(platform), "w") as fo:
             fo.write("# platform: {platform}\n".format(platform=platform))
             fo.write("@EXPLICIT\n")
-            prefix_setup = state.prefix_setups[PREFIX_NAME]
-            for ms in prefix_setup.link_precs:
-                fo.write(ms.url)
+            urls = {
+                fn_to_dist_name(pkg['fn']): pkg['url'] for pkg in dry_run_install["actions"]["FETCH"]
+            }
+            for pkg in dry_run_install["actions"]["LINK"]:
+                fo.write(urls[pkg["dist_name"]])
                 fo.write("\n")
 
     print("To use the generated lock files create a new environment:", file=sys.stderr)
@@ -126,7 +78,6 @@ def make_lock_files(platforms, channels, specs):
 
 
 if __name__ == "__main__":
-    reset_context()
     import argparse
 
     parser = argparse.ArgumentParser()
@@ -149,7 +100,7 @@ if __name__ == "__main__":
     environment_file = pathlib.Path(args.file)
     desired_env = parse_environment_file(environment_file)
     make_lock_files(
-        channels=desired_env["channels"] or context.channels,
+        channels=desired_env["channels"] or [],
         specs=desired_env["specs"],
         platforms=args.platform or DEFAULT_PLATFORMS,
     )
