03610298969a73948a2a661fbbcd9e64c331e39f
Filipe Fernandes <ocefpaf@gmail.com>
2020-02-27 18:44:03 -0300

    fix version

--- conda_lock/__init__.py
+++ conda_lock/__init__.py
@@ -1,4 +1,12 @@
+import pkg_resources
+
 from conda_lock.conda_lock import main
 
 
-__names__ = [main]
+__all__ = ["main"]
+
+
+try:
+    __version__ = pkg_resources.get_distribution("conda_lock").version
+except Exception:
+    __version__ = "unknown"
