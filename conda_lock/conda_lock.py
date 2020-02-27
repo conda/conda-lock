0e4c6b07b9e5ae2e0010ccb907a63bd91cddb228
Filipe Fernandes <ocefpaf@gmail.com>
2020-02-27 10:12:37 -0300

    type annotations

--- conda_lock/conda_lock.py
+++ conda_lock/conda_lock.py
@@ -1,30 +1,29 @@
-#!/usr/bin/env python3
 """
 Somewhat hacky solution to create conda lock files.
 """
 
 import json
+import os
+import pathlib
+import subprocess
 import sys
+import tempfile
+
+from typing import Dict, List
+
+import requests
+import yaml
+
 
 if not (sys.version_info.major >= 3 and sys.version_info.minor >= 6):
     print("conda_lock needs to run under python >=3.6")
     sys.exit(1)
 
-import pathlib
-import subprocess
-import sys
-import yaml
-import os
-import requests
-import tempfile
-
 
 DEFAULT_PLATFORMS = ["osx-64", "linux-64", "win-64"]
 
 
-def solve_specs_for_arch(channels, specs, platform):
-    # type: (typing.List[str], typing.List[str], str) -> dict
-
+def solve_specs_for_arch(channels: List[str], specs: List[str], platform: str) -> str:
     env = dict(os.environ)
     with tempfile.TemporaryDirectory() as CONDA_PKGS_DIRS:
         env.update({"CONDA_SUBDIR": platform, "CONDA_PKGS_DIRS": CONDA_PKGS_DIRS})
@@ -33,7 +32,7 @@ def solve_specs_for_arch(channels, specs, platform):
             "conda",
             "create",
             "--prefix",
-            pathlib.Path(CONDA_PKGS_DIRS).joinpath("prefix"),
+            str(pathlib.Path(CONDA_PKGS_DIRS).joinpath("prefix")),
             "--override-channels",
             "--dry-run",
             "--json",
@@ -46,8 +45,7 @@ def solve_specs_for_arch(channels, specs, platform):
     return json.loads(json_output)
 
 
-def parse_environment_file(environment_file):
-    # type: (pathlib.Path) -> list
+def parse_environment_file(environment_file: pathlib.Path) -> Dict:
     if not environment_file.exists():
         raise FileNotFoundError("{} not found".format(environment_file))
     with environment_file.open("r") as fo:
@@ -59,10 +57,11 @@ def parse_environment_file(environment_file):
     return {"specs": specs, "channels": channels}
 
 
-def fn_to_dist_name(fn):
-    fn, _, _ = fn.partition('.conda')
-    fn, _, _ = fn.partition('.tar.bz2')
+def fn_to_dist_name(fn: str) -> str:
+    fn, _, _ = fn.partition(".conda")
+    fn, _, _ = fn.partition(".tar.bz2")
     return fn
+
 
 def make_lock_files(platforms, channels, specs):
     for platform in platforms:
@@ -74,22 +73,26 @@ def make_lock_files(platforms, channels, specs):
             fo.write("# platform: {platform}\n".format(platform=platform))
             fo.write("@EXPLICIT\n")
             urls = {
-                fn_to_dist_name(pkg['fn']): pkg['url'] for pkg in dry_run_install["actions"]["FETCH"]
+                fn_to_dist_name(pkg["fn"]): pkg["url"]
+                for pkg in dry_run_install["actions"]["FETCH"]
             }
             md5s = {
-                fn_to_dist_name(pkg['fn']): pkg['md5'] for pkg in dry_run_install["actions"]["FETCH"]
+                fn_to_dist_name(pkg["fn"]): pkg["md5"]
+                for pkg in dry_run_install["actions"]["FETCH"]
             }
             for pkg in dry_run_install["actions"]["LINK"]:
                 url = urls[pkg["dist_name"]]
                 md5 = md5s[pkg["dist_name"]]
                 r = requests.head(url, allow_redirects=True)
                 url = r.url
-                fo.write(f'{url}#{md5}')
+                fo.write(f"{url}#{md5}")
                 fo.write("\n")
 
     print("To use the generated lock files create a new environment:", file=sys.stderr)
     print("", file=sys.stderr)
-    print("     conda create --name YOURENV --file conda-linux-64.lock", file=sys.stderr)
+    print(
+        "     conda create --name YOURENV --file conda-linux-64.lock", file=sys.stderr
+    )
     print("", file=sys.stderr)
 
 
@@ -120,6 +123,7 @@ def main():
         specs=desired_env["specs"],
         platforms=args.platform or DEFAULT_PLATFORMS,
     )
+
 
 if __name__ == "__main__":
     main()
