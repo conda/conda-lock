conda/conda-lock

commit 9aa90dc0b12c49785e912a1f87bfe38762e89058

Add a --check-input-hash flag
This will perform a check against already existing lockfiles determining
if the input hash of the spec has changed.  If the spec has not changed
then the lockfile is not regenerated.

This change makes some minor cleanup changed to the lockfile format for
increased clarity.  env_hash -> input_hash so its clearer what that hash
represents.

conda_lock/conda_lock.py
@@ -56,6 +56,8 @@ DOMAIN_PATTERN = re.compile(r"^(https?:\/\/)?([^\/]+)(.*)")
 
 # Captures the platform in the first group.
 PLATFORM_PATTERN = re.compile(r"^# platform: (.*)$")
+INPUT_HASH_PATTERN = re.compile(r"^# input_hash: (.*)$")
+
 
 if not (sys.version_info.major >= 3 and sys.version_info.minor >= 6):
     print("conda_lock needs to run under python >=3.6")
@@ -82,12 +84,27 @@ def _extract_platform(line: str) -> Optional[str]:
     return None
 
 
+def _extract_spec_hash(line: str) -> Optional[str]:
+    search = INPUT_HASH_PATTERN.search(line)
+    if search:
+        return search.group(1)
+    return None
+
+
 def extract_platform(lockfile: str) -> str:
     for line in lockfile.strip().split("\n"):
         platform = _extract_platform(line)
         if platform:
             return platform
     raise RuntimeError("Cannot find platform in lockfile.")
+
+
+def extract_input_hash(lockfile_contents: str) -> Optional[str]:
+    for line in lockfile_contents.strip().split("\n"):
+        platform = _extract_spec_hash(line)
+        if platform:
+            return platform
+    return None
 
 
 def _do_validate_platform(platform: str) -> Tuple[bool, str]:
@@ -181,6 +198,9 @@ def solve_specs_for_arch(
         except json.JSONDecodeError as e:
             print(f"Failed to parse json, {e}")
             message = ""
+        except KeyError:
+            print("Message key not found in json! returning the full json text")
+            message = err_json
 
         print(f"Could not lock the environment for platform {platform}")
         if message:
@@ -331,6 +351,32 @@ def fn_to_dist_name(fn: str) -> str:
     return fn
 
 
+def make_lock_specs(
+    *,
+    platforms: List[str],
+    src_files: List[pathlib.Path],
+    include_dev_dependencies: bool = True,
+    channel_overrides: Optional[Sequence[str]] = None,
+) -> Dict[str, LockSpecification]:
+    """Generate the lockfile specs from a set of input src_files"""
+    res = {}
+    for plat in platforms:
+        lock_specs = parse_source_files(
+            src_files=src_files,
+            platform=plat,
+            include_dev_dependencies=include_dev_dependencies,
+        )
+
+        lock_spec = aggregate_lock_specs(lock_specs)
+        if channel_overrides:
+            channels = list(channel_overrides)
+        else:
+            channels = lock_spec.channels
+        lock_spec.channels = channels
+        res[sys.platform] = lock_spec
+    return res
+
+
 def make_lock_files(
     conda: PathLike,
     platforms: List[str],
@@ -339,6 +385,7 @@ def make_lock_files(
     include_dev_dependencies: bool = True,
     channel_overrides: Optional[Sequence[str]] = None,
     filename_template: Optional[str] = None,
+    check_spec_hash: bool = False,
 ):
     """Generate the lock files for the given platforms from the src file provided
 
@@ -356,6 +403,8 @@ def make_lock_files(
         Forced list of channels to use.
     filename_template :
         Format for the lock file names. Must include {platform}.
+    check_spec_hash :
+        Validate that the existing spec hash has not already been generated for.
 
     """
     if filename_template:
@@ -375,33 +424,22 @@ def make_lock_files(
                 )
                 sys.exit(1)
 
-    for plat in platforms:
-        print(f"Generating lockfile(s) for {plat}...", file=sys.stderr)
-        lock_specs = parse_source_files(
-            src_files=src_files,
-            platform=plat,
-            include_dev_dependencies=include_dev_dependencies,
-        )
+    lock_specs = make_lock_specs(
+        platforms=platforms,
+        src_files=src_files,
+        include_dev_dependencies=include_dev_dependencies,
+        channel_overrides=channel_overrides,
+    )
 
-        lock_spec = aggregate_lock_specs(lock_specs)
-        if channel_overrides:
-            channels = channel_overrides
-        else:
-            channels = lock_spec.channels
-
+    for plat, lock_spec in lock_specs.items():
         for kind in kinds:
-            lockfile_contents = create_lockfile_from_spec(
-                channels=channels,
-                conda=conda,
-                spec=lock_spec,
-                kind=kind,
-            )
-
             if filename_template:
                 context = {
                     "platform": lock_spec.platform,
                     "dev-dependencies": str(include_dev_dependencies).lower(),
-                    "spec-hash": lock_spec.env_hash(),
+                    # legacy key
+                    "spec-hash": lock_spec.input_hash(),
+                    "input-hash": lock_spec.input_hash(),
                     "version": pkg_resources.get_distribution("conda_lock").version,
                     "timestamp": datetime.datetime.utcnow().strftime("%Y%m%dT%H%M%SZ"),
                 }
@@ -409,6 +447,24 @@ def make_lock_files(
                 filename = filename_template.format(**context)
             else:
                 filename = f"conda-{lock_spec.platform}.lock"
+
+            lockfile = pathlib.Path(filename)
+            if lockfile.exists() and check_spec_hash:
+                existing_spec_hash = extract_input_hash(lockfile.read_text())
+                if existing_spec_hash == lock_spec.input_hash():
+                    print(
+                        f"Spec hash already locked for {plat}. Skipping",
+                        file=sys.stderr,
+                    )
+                    continue
+
+            print(f"Generating lockfile(s) for {plat}...", file=sys.stderr)
+            lockfile_contents = create_lockfile_from_spec(
+                channels=lock_spec.channels,
+                conda=conda,
+                spec=lock_spec,
+                kind=kind,
+            )
 
             filename += KIND_FILE_EXT[kind]
             with open(filename, "w") as fo:
@@ -455,7 +511,7 @@ def create_lockfile_from_spec(
     lockfile_contents = [
         "# Generated by conda-lock.",
         f"# platform: {spec.platform}",
-        f"# env_hash: {spec.env_hash()}\n",
+        f"# input_hash: {spec.input_hash()}\n",
     ]
 
     if kind == "env":
@@ -694,6 +750,7 @@ def run_lock(
     channel_overrides: Optional[Sequence[str]] = None,
     filename_template: Optional[str] = None,
     kinds: Optional[List[str]] = None,
+    check_input_hash: bool = False,
 ) -> None:
     if environment_files == DEFAULT_FILES:
         long_ext_file = pathlib.Path("environment.yaml")
@@ -711,6 +768,7 @@ def run_lock(
         channel_overrides=channel_overrides,
         filename_template=filename_template,
         kinds=kinds or DEFAULT_KINDS,
+        check_spec_hash=check_input_hash,
     )
 
 
@@ -780,6 +838,12 @@ def main():
     help="Strip the basic auth credentials from the lockfile.",
 )
 @click.option(
+    "--check-input-hash",
+    is_flag=True,
+    default=False,
+    help="Check existing input hashes in lockfiles before regenerating lock files.  If no files were updated exit with exit code 4.  Incompatible with --strip-auth",
+)
+@click.option(
     "--log-level",
     help="Log level.",
     default="INFO",
@@ -806,6 +870,7 @@ def lock(
     kind,
     filename_template,
     strip_auth,
+    check_input_hash: bool,
     log_level,
 ):
     """Generate fully reproducible lock files for conda environments.
@@ -816,7 +881,7 @@ def lock(
     \b
         platform: The platform this lock file was generated for (conda subdir).
         dev-dependencies: Whether or not dev dependencies are included in this lock file.
-        spec-hash: A sha256 hash of the lock file spec.
+        input-hash: A sha256 hash of the lock file input specification.
         version: The version of conda-lock used to generate this lock file.
         timestamp: The approximate timestamp of the output file in ISO8601 basic format.
     """
@@ -843,7 +908,9 @@ def lock(
                 lockfile = _strip_auth_from_lockfile(lockfile)
                 write_file(lockfile, os.path.join(filename_template_dir, file))
     else:
-        lock_func(filename_template=filename_template)
+        lock_func(
+            filename_template=filename_template, check_input_hash=check_input_hash
+        )
 
 
 @main.command("install")

.pre-commit-config.yaml
@@ -5,24 +5,25 @@ repos:
     - id: trailing-whitespace
     - id: check-ast
 
+- repo: https://github.com/psf/black
+  rev: 21.8b0
+  hooks:
+  - id: black
+    language_version: python3
+
 - repo: https://gitlab.com/pycqa/flake8
   rev: 3.9.2
   hooks:
     - id: flake8
 
 - repo: https://github.com/pycqa/isort
-  rev: 5.8.0
+  rev: 5.9.3
   hooks:
   - id: isort
     args: ["--profile", "black", "--filter-files"]
 
-- repo: https://github.com/psf/black
-  rev: 21.5b1
-  hooks:
-  - id: black
-    language_version: python3
-
 - repo: https://github.com/pre-commit/mirrors-mypy
-  rev: v0.812
+  rev: v0.910
   hooks:
   - id: mypy
+    additional_dependencies: [types-requests, types-toml, types-PyYAML, types-setuptools]

conda-linux-64-true.lock.yml
@@ -1,0 +1,13 @@
+# Generated by conda-lock.
+# platform: linux-64
+# input_hash: 3785fe554e946f6fcbd195fa2ce80493621fe6798c631d2e00b8df3f7c3c2282
+
+channels:
+  - conda-forge
+  - defaults
+dependencies:
+  - _libgcc_mutex=0.1=conda_forge
+  - libgomp=11.1.0=hc902ee8_8
+  - _openmp_mutex=4.5=1_gnu
+  - libgcc-ng=11.1.0=hc902ee8_8
+  - zlib=1.2.11=h516909a_1010

conda_lock/src_parser/__init__.py
@@ -10,7 +10,7 @@ class LockSpecification:
         self.channels = channels
         self.platform = platform
 
-    def env_hash(self) -> str:
+    def input_hash(self) -> str:
         env_spec = json.dumps(
             {
                 "channels": self.channels,

tests/test-input-hash-zlib/.gitignore
@@ -1,0 +1,1 @@
+*.lock

tests/test-input-hash-zlib/environment.yml
@@ -1,0 +1,5 @@
+channels:
+    - conda-forge
+    - defaults
+dependencies:
+    - zlib

tests/test_conda_lock.py
@@ -53,6 +53,15 @@ def zlib_environment():
 
 
 @pytest.fixture
+def input_hash_zlib_environment():
+    return (
+        pathlib.Path(__file__)
+        .parent.joinpath("test-input-hash-zlib")
+        .joinpath("environment.yml")
+    )
+
+
+@pytest.fixture
 def meta_yaml_environment():
     return pathlib.Path(__file__).parent.joinpath("test-recipe").joinpath("meta.yaml")
 
@@ -140,6 +149,38 @@ def test_run_lock(monkeypatch, zlib_environment, conda_exe):
     run_lock([zlib_environment], conda_exe=conda_exe)
 
 
+def test_run_lock_with_input_hash_check(
+    monkeypatch, input_hash_zlib_environment: pathlib.Path, conda_exe, capsys
+):
+    monkeypatch.chdir(input_hash_zlib_environment.parent)
+    if is_micromamba(conda_exe):
+        monkeypatch.setenv("CONDA_FLAGS", "-v")
+    lockfile = input_hash_zlib_environment.parent / "conda-linux-64.lock"
+    if lockfile.exists():
+        lockfile.unlink()
+
+    run_lock(
+        [input_hash_zlib_environment],
+        platforms=["linux-64"],
+        conda_exe=conda_exe,
+        check_input_hash=True,
+    )
+    stat = lockfile.stat()
+    created = stat.st_mtime_ns
+
+    capsys.readouterr()
+    run_lock(
+        [input_hash_zlib_environment],
+        platforms=["linux-64"],
+        conda_exe=conda_exe,
+        check_input_hash=True,
+    )
+    stat = lockfile.stat()
+    assert stat.st_mtime_ns == created
+    output = capsys.readouterr()
+    assert "Spec hash already locked for" in output.err
+
+
 @pytest.mark.parametrize(
     "package,version,url_pattern",
     [
@@ -183,21 +224,21 @@ def test_aggregate_lock_specs():
     )
 
     assert (
-        aggregate_lock_specs([gpu_spec, base_spec]).env_hash()
+        aggregate_lock_specs([gpu_spec, base_spec]).input_hash()
         == LockSpecification(
             specs=["pytorch", "python =3.7"],
             channels=["pytorch", "conda-forge"],
             platform="linux-64",
-        ).env_hash()
+        ).input_hash()
     )
 
     assert (
-        aggregate_lock_specs([base_spec, gpu_spec]).env_hash()
+        aggregate_lock_specs([base_spec, gpu_spec]).input_hash()
         == LockSpecification(
             specs=["pytorch", "python =3.7"],
             channels=["conda-forge"],
             platform="linux-64",
-        ).env_hash()
+        ).input_hash()
     )
 
 

commit 1a6024089b38e055c7f816741ad4189deaca0965

Fix regression for multi-platform lock
Regression was  introduced in 9aa90dc0b12c49785e912a1f87bfe38762e89058: `make_lock_specs` would write all resolved specs to the key `sys.platform`.
This results in only the first platform being processed. There are no error messages and only hint of something being amiss is that logging reports the lockfile being built for the value of `sys.platform` instead of the actual platform.

conda_lock/conda_lock.py
@@ -373,7 +373,7 @@ def make_lock_specs(
         else:
             channels = lock_spec.channels
         lock_spec.channels = channels
-        res[sys.platform] = lock_spec
+        res[plat] = lock_spec
     return res
 
 