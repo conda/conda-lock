13bd0c7b2fb31f8e23f2f3fe3a3f3449785d5cbf
Marius van Niekerk <marius.v.niekerk@gmail.com>
2022-02-17 17:14:04 -0500

    Vendor conda

---
+++ conda_lock/vendor/README.md
@@ -0,0 +1,4 @@
+This contains an entire vendored copy of conda so that we can make use of
+
+* conda.models.MatchSpec
+* conda.
---
+++ conda_lock/vendor/conda/__main__.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+# Copyright (C) 2012 Anaconda, Inc
+# SPDX-License-Identifier: BSD-3-Clause
+import sys
+
+from .cli import main
+
+sys.exit(main())
