7638c0cd19f4d18349792f1b6433913663ea84fc
Marius van Niekerk <marius.v.niekerk@gmail.com>
2022-07-10 15:30:06 -0400

    Add some more documentation around the tests

--- conda_lock/src_parser/conda_common.py
+++ conda_lock/src_parser/conda_common.py
@@ -2,10 +2,14 @@ from typing import Optional
 
 from ..src_parser import VersionedDependency
 from ..vendor.conda.models.channel import Channel
+from ..vendor.conda.models.match_spec import MatchSpec
 
 
 def conda_spec_to_versioned_dep(spec: str, category: str) -> VersionedDependency:
-    from ..vendor.conda.models.match_spec import MatchSpec
+    """Convert a string form conda spec into a versioned dependency for a given category.
+
+    This is used by the environment.yaml and meta.yaml specification parser
+    """
 
     try:
         ms = MatchSpec(spec)
