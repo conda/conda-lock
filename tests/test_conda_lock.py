dd39b600d36262c9084d6b0be96310a50fb0ddad
Marius van Niekerk <marius.v.niekerk@gmail.com>
2023-05-17 11:35:10 -0400

    Fixup test

--- tests/test_conda_lock.py
+++ tests/test_conda_lock.py
@@ -14,7 +14,7 @@ import uuid
 
 from glob import glob
 from pathlib import Path
-from typing import Any, Dict, List
+from typing import Any, Dict, List, Union
 from unittest.mock import MagicMock
 from urllib.parse import urldefrag, urlsplit
 
@@ -103,8 +103,10 @@ def reset_global_conda_pkgs_dir():
     reset_conda_pkgs_dir()
 
 
-def clone_test_dir(name: str, tmp_path: Path) -> Path:
-    test_dir = TEST_DIR.joinpath(name)
+def clone_test_dir(name: Union[str, List[str]], tmp_path: Path) -> Path:
+    if isinstance(name, str):
+        name = [name]
+    test_dir = TEST_DIR.joinpath(*name)
     assert test_dir.exists()
     assert test_dir.is_dir()
     if sys.version_info >= (3, 8):
@@ -1365,16 +1367,16 @@ def test_aggregate_lock_specs_invalid_channels():
 @pytest.mark.parametrize(
     ["test_dir", "filename"],
     [
-        ("test-pypi-resolve-gh290/pyproject", "pyproject.toml"),
-        ("test-pypi-resolve-gh290/tzdata", "environment.yaml"),
-        ("test-pypi-resolve-gh290/wdl", "environment.yaml"),
+        (["test-pypi-resolve-gh290", "pyproject"], "pyproject.toml"),
+        (["test-pypi-resolve-gh290", "tzdata"], "environment.yaml"),
+        (["test-pypi-resolve-gh290", "wdl"], "environment.yaml"),
     ],
 )
 def test_conda_pip_regressions_gh290(
     tmp_path: Path,
     mamba_exe: str,
     monkeypatch: "pytest.MonkeyPatch",
-    test_dir: str,
+    test_dir: List[str],
     filename: str,
 ):
     """Simple test that asserts that these engieonments can be locked"""
