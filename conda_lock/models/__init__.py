b249119e78d5c68625cc656e29480ad58ed3ad2d
Marius van Niekerk <marius.v.niekerk@gmail.com>
2023-07-13 16:47:37 -0500

    Update to pydantic 2.0

--- conda_lock/models/__init__.py
+++ conda_lock/models/__init__.py
@@ -1,11 +1,5 @@
 from pydantic import BaseModel
 
 
-class StrictModel(BaseModel):
+class StrictModel(BaseModel, extra="forbid"):
     """A Pydantic BaseModel forbidding extra fields and encoding frozensets as lists"""
-
-    class Config:
-        extra = "forbid"
-        json_encoders = {
-            frozenset: list,
-        }
