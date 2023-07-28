41cae9a2fd516f29ae283231cd57d0c4513d4e0f
Stefan Sullivan <stefan.sullivan@gmail.com>
2023-07-28 13:31:21 -0700

    update requirements_txt parsing to return valid Dependency objects

--- conda_lock/src_parser/environment_yaml.py
+++ conda_lock/src_parser/environment_yaml.py
@@ -4,11 +4,10 @@ import tempfile
 from typing import List, Tuple
 
 import yaml
-from packaging._parser import parse_requirement
 
 from conda_lock.models.lock_spec import Dependency, LockSpecification
 from conda_lock.src_parser.conda_common import conda_spec_to_versioned_dep
-from conda_lock.src_parser.requirements_txt import parse_requirements_txt
+from conda_lock.src_parser.requirements_txt import parse_requirements_txt, parse_one_requirement
 from conda_lock.src_parser.selectors import filter_platform_selectors
 
 _whitespace = re.compile(r"\s+")
@@ -27,6 +26,7 @@ def _parse_environment_file_for_platform(
     content: str,
     category: str,
     platform: str,
+    source_path: pathlib.Path,
 ) -> List[Dependency]:
     """
     Parse dependencies from a conda environment specification for an
@@ -54,15 +54,17 @@ def _parse_environment_file_for_platform(
     for mapping_spec in mapping_specs:
         if "pip" in mapping_spec:
             # Generate the temporary requirements file
-            with tempfile.NamedTemporaryFile(suffix="requirements.txt", mode='w', encoding='utf-8') as requirements:
+            yaml_dir = source_path.parent
+            with tempfile.NamedTemporaryFile(dir=yaml_dir, suffix="requirements.txt", mode='w', encoding='utf-8') as requirements:
                 requirements.writelines(mapping_spec["pip"])
-                requirements.close()
+                requirements.write("\n")  # Trailing newline
+                requirements.file.close()
 
-                dependencies.extend(filter(None, parse_requirements_txt(requirements.name)))
+                dependencies.extend(parse_requirements_txt(requirements.name, category))
 
             # ensure pip is in target env
             if 'pip' not in {d.name for d in dependencies}:
-                dependencies.append(parse_requirement("pip"))
+                dependencies.append(parse_one_requirement("pip", category))
 
     return dependencies
 
@@ -111,7 +113,7 @@ def parse_environment_file(
 
     # Parse with selectors for each target platform
     dep_map = {
-        platform: _parse_environment_file_for_platform(content, category, platform)
+        platform: _parse_environment_file_for_platform(content, category, platform, environment_file)
         for platform in platforms
     }
 
