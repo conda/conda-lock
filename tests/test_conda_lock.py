50939243a6ee580409891c0e69d17e3dc0a64e11
Romain <romain-intel@users.noreply.github.com>
2023-12-08 14:51:33 -0800

    Apply suggestions from code review
    Co-authored-by: Ben Mares <services-git-throwaway1@tensorial.com>

--- tests/test_conda_lock.py
+++ tests/test_conda_lock.py
@@ -2478,7 +2478,14 @@ def test_pip_full_whl_url(
     lockfile = parse_conda_lock_file(env_file.parent / DEFAULT_LOCKFILE_NAME)
 
     (requests_dep,) = [p for p in lockfile.package if p.name == "requests"]
+    (typing_extensions_dep,) = [p for p in lockfile.package if p.name == "typing-extensions"]
     assert (
         requests_dep.url
         == "https://github.com/psf/requests/releases/download/v2.31.0/requests-2.31.0-py3-none-any.whl"
     )
+    assert requests_dep.hash.sha256 is None
+    assert (
+        typing_extensions_dep.url
+        == "https://files.pythonhosted.org/packages/24/21/7d397a4b7934ff4028987914ac1044d3b7d52712f30e2ac7a2ae5bc86dd0/typing_extensions-4.8.0-py3-none-any.whl"
+    )
+    assert typing_extensions_dep.hash.sha256 == "8f92fc8806f9a6b641eaa5318da32b44d401efaac0f6678c9bc448ba3605faa0"
