589987ead4207effeb82329d57f86b2da4bb3d73
Matthew R. Becker <beckermr@users.noreply.github.com>
2026-04-08 06:22:15 -0500

    Refactor plugin to handle optional conda import

--- conda_lock/plugin.py
+++ conda_lock/plugin.py
@@ -6,24 +6,30 @@ subcommand when this package is installed in the same environment as conda.
 
 from __future__ import annotations
 
-from conda.plugins import hookimpl
-from conda.plugins.types import CondaSubcommand
+try:
+    from conda.plugins import hookimpl
+    from conda.plugins.types import CondaSubcommand
+
+    HAVE_CONDA = True
+except ImportError:
+    HAVE_CONDA = False
 
 
-def _execute(args: tuple[str, ...]) -> int | None:
-    """Dispatch plugin arguments to the lock CLI.
+if HAVE_CONDA:
+    def _execute(args: tuple[str, ...]) -> int | None:
+        """Dispatch plugin arguments to the lock CLI.
 
-    Lazy import to avoid import-time side effects when not using conda-lock.
-    """
-    from conda_lock.__main__ import main
+        Lazy import to avoid import-time side effects when not using conda-lock.
+        """
+        from conda_lock.__main__ import main
 
-    return main()  # TODO: does not accept args for parsing since using click
+        return main()  # TODO: does not accept args for parsing since using click
 
 
-@hookimpl
-def conda_subcommands():
-    yield CondaSubcommand(
-        name="lock",
-        summary=("enerate fully reproducible lock files for conda environments."),
-        action=_execute,
-    )
+    @hookimpl
+    def conda_subcommands():
+        yield CondaSubcommand(
+            name="lock",
+            summary=("enerate fully reproducible lock files for conda environments."),
+            action=_execute,
+        )
